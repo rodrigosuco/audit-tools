3bafc92ff87fa35bbd715836d12681607cc349fe
rodrigo <rod1dev@proton.me>
2024-10-13 23:38:47 -0300

    feat: add badges

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -3,10 +3,17 @@ class Proposal < ApplicationRecord
   belongs_to :company
   before_save :calc_total_cost
 
+  def last_updated_at
+    return ((Time.now - self.updated_at) / 1.day).to_i if self.items.empty?
+    most_recent_item = self.items.max_by(&:updated_at)
+    ((Time.now - most_recent_item.updated_at) / 1.day).to_i
+  end
+
   private
 
   def calc_total_cost
     return unless self.items.present?
     self.total_cost = self.items.map(&:total_cost).inject(:+).round(2)
   end
+
 end
